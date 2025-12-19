65158fbce12a96c012fcbd756cf6c1984d7058fa
Dyadina <nehruslan12@gmail.com>
2025-12-20 01:05:41 +0200

    Fix USER_NAME error in Dyadina/GitHubUserStatistics.java

--- Dyadina/GitHubUserStatistics.java
+++ Dyadina/GitHubUserStatistics.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -6,7 +5,7 @@ import java.net.URL;
 
 
 public class GitHubUserStatistics {
-    public final static String USER_NAME;
+    public final static String USER_NAME = "Dyadina";
 
 
     public static void main(String[] args) {
